10f44823ca0489bd6ef9f6c000c16f4b8b93ac70
Miriam Elizabeth Laguna Velazquez <Miriam Elizabeth Laguna Velazquez>
2017-08-10 00:58:29 -0500

    primera parte

--- public/js/validaciones.js
+++ public/js/validaciones.js
@@ -1,13 +1,22 @@
-$(document).ready(
-	cargarPagina();
-);
-
-var nombre = $("#nombre").val();
-var apellido = $("#apellido").val();
+var nombre = $("#first_name");
+var apellido = $("#last_name");
 var cargarPagina = function(){
-
+	nombre.keyup(validacionNombres);
+	apellido.keyup(validacionNombres);
+};
+var validacionNombres = function (e){
+	e.preventDefault();
+	if(e.keyCode >= 48 && e.keyCode <= 57){
+		$(this).removeClass('validate');
+		$(this).addClass('invalid');
+	}
+	else if(e.keyCode >= 65 && e.keyCode <= 90 || e.keyCode >= 97 && e.keyCode <= 122 && e.keyCode == 46){
+		$(this).removeClass('invalid');
+		$(this).addClass('validate');
+	}
 };
 
-var validacionNombre = function(){
-	if(var i = 0)
-};
+
+$(document).ready(function(){
+	cargarPagina();
+});
